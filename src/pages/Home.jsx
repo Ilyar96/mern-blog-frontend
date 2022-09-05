25c7353b24dbdf2054e679a7bdcfbc62ab0a32ce
Ilyar <iljar646@gmail.com>
2022-09-05 10:29:20 +0300

    posts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,82 @@
-import React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Grid from '@mui/material/Grid';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Grid from "@mui/material/Grid";
 
-import { Post } from '../components/Post';
-import { TagsBlock } from '../components/TagsBlock';
-import { CommentsBlock } from '../components/CommentsBlock';
+import { Post } from "../components/Post";
+import { TagsBlock } from "../components/TagsBlock";
+import { CommentsBlock } from "../components/CommentsBlock";
+import { fetchPosts, fetchTags } from "../redux/slices/posts";
+import { getCorrectTime, getCorrectDate } from "../utils/getCorrectDate";
 
 export const Home = () => {
+  const dispatch = useDispatch();
+  const { posts, tags } = useSelector((state) => state.posts);
+
+  const isPostLoading = posts.status === "loading";
+  const istagsLoading = tags.status === "loading";
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+    dispatch(fetchTags());
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <>
-      <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
+      <Tabs
+        style={{ marginBottom: 15 }}
+        value={0}
+        aria-label="basic tabs example"
+      >
         <Tab label="Новые" />
         <Tab label="Популярные" />
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {[...Array(5)].map(() => (
-            <Post
-              id={1}
-              title="Roast the code #1 | Rock Paper Scissors"
-              imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-              user={{
-                avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
-              }}
-              createdAt={'12 июня 2022 г.'}
-              viewsCount={150}
-              commentsCount={3}
-              tags={['react', 'fun', 'typescript']}
-              isEditable
-            />
-          ))}
+          {(isPostLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+            isPostLoading ? (
+              <Post key={index} isLoading={true} />
+            ) : (
+              <Post
+                id={obj._id}
+                title={obj.title}
+                imageUrl={
+                  obj.imageUrl
+                    ? obj.imageUrl
+                    : "https://img.freepik.com/free-photo/womans-hands-typing-on-laptop-at-workplace_1163-4324.jpg?w=1380&t=st=1662362341~exp=1662362941~hmac=4f6d265197d9f6dd440fd47dabc277a4c413556cd430b2e7bc7cc596a48b4cb3"
+                }
+                user={obj.user}
+                createdAt={`
+									${getCorrectDate(obj.createdAt)} 
+									${getCorrectTime(obj.createdAt)}
+								`}
+                viewsCount={obj.viewsCount}
+                commentsCount={3}
+                tags={obj.tags}
+                isEditable
+              />
+            )
+          )}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={tags.items} isLoading={istagsLoading} />
           <CommentsBlock
             items={[
               {
                 user: {
-                  fullName: 'Вася Пупкин',
-                  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+                  fullName: "Вася Пупкин",
+                  avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
                 },
-                text: 'Это тестовый комментарий',
+                text: "Это тестовый комментарий",
               },
               {
                 user: {
-                  fullName: 'Иван Иванов',
-                  avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                  fullName: "Иван Иванов",
+                  avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
                 },
-                text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
+                text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
               },
             ]}
             isLoading={false}
---
+++ src/utils/getCorrectDate.js
@@ -0,0 +1,3 @@
+export const getCorrectDate = (date) => date.slice(0, 10);
+
+export const getCorrectTime = (date) => date.slice(11, 16);
