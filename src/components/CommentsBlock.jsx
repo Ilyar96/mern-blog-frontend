4117334dea57bcbdb38885bc5e84b7d47e1ab9df
Ilyar <iljar646@gmail.com>
2022-09-18 17:15:28 +0300

    fix

--- src/components/CommentsBlock.jsx
+++ src/components/CommentsBlock.jsx
@@ -13,12 +13,13 @@ import { IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Clear";
 import { useConfirm } from "material-ui-confirm";
 
-import { selectUser } from "../redux/services/authSlice";
+import { selectUser, selectIsAuth } from "../redux/services/authSlice";
 import Modal from "./Modal";
 import { useDeleteCommentMutation } from "../redux/services/post";
 
 export const CommentsBlock = ({ items, children, isLoading, commentsRef }) => {
   const user = useSelector(selectUser);
+  const isAuth = useSelector(selectIsAuth);
   const confirm = useConfirm();
   const [deleteComment, { isError }] = useDeleteCommentMutation();
   const comments = items ? items : [];
@@ -43,50 +44,52 @@ export const CommentsBlock = ({ items, children, isLoading, commentsRef }) => {
 
   return (
     <>
-      <SideBlock title="Комментарии">
-        {children}
-        <List ref={commentsRef}>
-          {(isLoading ? [...Array(5)] : comments).map((obj, index) => (
-            <React.Fragment key={index}>
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  {isLoading ? (
-                    <Skeleton variant="circular" width={40} height={40} />
-                  ) : (
-                    <Avatar
-                      alt={obj?.user?.fullName}
-                      src={obj?.user?.avatarUrl}
-                    />
-                  )}
-                </ListItemAvatar>
-                {isLoading ? (
-                  <div style={{ display: "flex", flexDirection: "column" }}>
-                    <Skeleton variant="text" height={25} width={120} />
-                    <Skeleton variant="text" height={18} width={230} />
-                  </div>
-                ) : (
-                  <>
-                    <ListItemText
-                      primary={obj?.user.fullName}
-                      secondary={obj?.text}
-                    />
-                    {user?._id === obj?.user._id && (
-                      <IconButton
-                        onClick={() => onClickRemove(obj?._id)}
-                        color="secondary"
-                        title={"Удалить"}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
+      {(isAuth || comments.length > 0) && (
+        <SideBlock title="Комментарии">
+          {children}
+          <List ref={commentsRef}>
+            {(isLoading ? [...Array(5)] : comments).map((obj, index) => (
+              <React.Fragment key={index}>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    {isLoading ? (
+                      <Skeleton variant="circular" width={40} height={40} />
+                    ) : (
+                      <Avatar
+                        alt={obj?.user?.fullName}
+                        src={obj?.user?.avatarUrl}
+                      />
                     )}
-                  </>
-                )}
-              </ListItem>
-              <Divider variant="inset" component="li" />
-            </React.Fragment>
-          ))}
-        </List>
-      </SideBlock>
+                  </ListItemAvatar>
+                  {isLoading ? (
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                      <Skeleton variant="text" height={25} width={120} />
+                      <Skeleton variant="text" height={18} width={230} />
+                    </div>
+                  ) : (
+                    <>
+                      <ListItemText
+                        primary={obj?.user.fullName}
+                        secondary={obj?.text}
+                      />
+                      {user?._id === obj?.user._id && (
+                        <IconButton
+                          onClick={() => onClickRemove(obj?._id)}
+                          color="secondary"
+                          title={"Удалить"}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      )}
+                    </>
+                  )}
+                </ListItem>
+                <Divider variant="inset" component="li" />
+              </React.Fragment>
+            ))}
+          </List>
+        </SideBlock>
+      )}
       <Modal isOpen={isError} text={"Не удалось удалить комментарий"} />
     </>
   );
